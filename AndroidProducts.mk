dbcac302a4412261623ca5a98e48227fc01514f6
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-10-25 15:53:06 +0530

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/derp_oxygen.mk
+
+COMMON_LUNCH_CHOICES := \
+    derp_oxygen-user \
+    derp_oxygen-userdebug \
+    derp_oxygen-eng
